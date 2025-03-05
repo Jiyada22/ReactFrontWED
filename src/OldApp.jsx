69afbb973cc4e1b09790e7a3910dd2ea1ae44c5a
Jiyada22 <jiyadakreenawong@gmail.com>
2025-03-05 09:49:17 +0700

    done

--- src/OldApp.jsx
+++ src/OldApp.jsx
@@ -1,27 +1,12 @@
-import Contact from "./assets/Contact";
-import Hello from "./assets/Hello";
-import Counter from "./assets/Counter";
+import Form from "./assets/Form";
 
-function App() {
-  const helloData = [
-    { name: 'Jiyada', message: 'Hi'},
-    { name: 'Cartoon', message: 'Hello..'},
-
-  ];
-
-  return(
-    <div className='App' >
-      <Counter />
-      {helloData.map((data, index) => (
-        <Hello key={index} name={data.name} message={data.message} />
-      ))}
-
-      < Contact email="s6506021631018@email.kmutnb.ac.th" phone="0988267055" />
-    </div>
-
-  );
+function App () {
+    return (
+        <div>
+            <h1>App Form</h1>
+            <Form />
+        </div>
+    );
 }
-
-  
 
 export default App;
